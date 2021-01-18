4ce3137ff02a4f4f8dbe5ac40235dae1de047d99
Pavlo Hilei <p.hilei@ucu.edu.ua>
2021-01-18 11:21:26 +0200

    Add stm32 projects

---
+++ STM32/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/DependInfo.cmake
@@ -0,0 +1,96 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/pasha/STMProjects/diodes/Core/Startup/startup_stm32f411vetx.s" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Core/Startup/startup_stm32f411vetx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../USB_HOST/App"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Core/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../USB_HOST/Target"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/STM32_USB_Host_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pasha/STMProjects/diodes/Core/Src/main.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Core/Src/main.c.obj"
+  "/home/pasha/STMProjects/diodes/Core/Src/stm32f4xx_hal_msp.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "/home/pasha/STMProjects/diodes/Core/Src/stm32f4xx_it.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "/home/pasha/STMProjects/diodes/Core/Src/syscalls.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Core/Src/syscalls.c.obj"
+  "/home/pasha/STMProjects/diodes/Core/Src/sysmem.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Core/Src/sysmem.c.obj"
+  "/home/pasha/STMProjects/diodes/Core/Src/system_stm32f4xx.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s_ex.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s_ex.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/pasha/STMProjects/diodes/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj"
+  "/home/pasha/STMProjects/diodes/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src/usbh_cdc.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src/usbh_cdc.c.obj"
+  "/home/pasha/STMProjects/diodes/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_core.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_core.c.obj"
+  "/home/pasha/STMProjects/diodes/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c.obj"
+  "/home/pasha/STMProjects/diodes/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ioreq.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ioreq.c.obj"
+  "/home/pasha/STMProjects/diodes/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_pipes.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_pipes.c.obj"
+  "/home/pasha/STMProjects/diodes/USB_HOST/App/usb_host.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/USB_HOST/App/usb_host.c.obj"
+  "/home/pasha/STMProjects/diodes/USB_HOST/Target/usbh_conf.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/USB_HOST/Target/usbh_conf.c.obj"
+  "/home/pasha/STMProjects/diodes/USB_HOST/Target/usbh_platform.c" "/home/pasha/STMProjects/diodes/cmake-build-debug/CMakeFiles/diodes.elf.dir/USB_HOST/Target/usbh_platform.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../USB_HOST/App"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Core/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../USB_HOST/Target"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/STM32_USB_Host_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
